047b15d5a1f096338133f9da199f6fac46b68d32
MR-JUMA <chachamaro1@yahoo.com>
2021-01-19 08:38:10 +0300

    Customer and Address models

--- abc/src/main/java/com/example/abc/models/Customer.java
+++ abc/src/main/java/com/example/abc/models/Customer.java
@@ -1,13 +1,62 @@
 package com.example.abc.models;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.Set;
 
 @Entity
 public class Customer {
     @Id
+    @GeneratedValue
     private int CustID;
     private String FirstName;
+    private String MiddleName;
     private String LastName;
 
+    @OneToMany
+    private Set<Address> address;
+
+    public int getCustID() {
+        return CustID;
+    }
+
+    public void setCustID(int custID) {
+        CustID = custID;
+    }
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public void setFirstName(String firstName) {
+        FirstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return MiddleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        MiddleName = middleName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String lastName) {
+        LastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "CustID=" + CustID +
+                ", FirstName='" + FirstName + '\'' +
+                ", MiddleName='" + MiddleName + '\'' +
+                ", LastName='" + LastName + '\'' +
+                '}';
+    }
 }
