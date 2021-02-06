03243f00a47983cf159cd298b4cf269e9cdd6966
MR-JUMA <chachamaro1@yahoo.com>
2021-02-06 16:25:13 +0300

    delete unneeded files

--- abc/src/main/java/com/example/abc/exceptions/CancelBookTooLateException.java
+++
@@ -1,8 +0,0 @@
-package com.example.abc.exceptions;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "cancel_booked_too_late")
-public class CancelBookTooLateException extends RuntimeException {
-}
