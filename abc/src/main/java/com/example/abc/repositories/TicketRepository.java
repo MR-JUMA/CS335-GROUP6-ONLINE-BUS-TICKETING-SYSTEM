9434d72742610628eb6b7ae09dd025660ff38228
justice1604 <justicedivine87@gmail.com>
2021-02-08 01:00:39 +0300

    repository.java

---
+++ abc/src/main/java/com/example/abc/repositories/TicketRepository.java
@@ -0,0 +1,12 @@
+package com.example.abc.repositories;
+
+import com.example.abc.models.Ticket;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TicketRepository extends JpaRepository<Ticket, Long> {
+    List<Ticket> findByUserId(Long userId);
+}
